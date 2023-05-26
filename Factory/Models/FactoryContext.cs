c64fb85c4e2e10a6c2a1ae42b4b9c6949842d988
Emma Gerigscott <elgerig@gmail.com>
2023-05-26 09:17:27 -0700

    add using directives to models

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -6,6 +6,6 @@ namespace Factory.Models{
     public DbSet<Machine> Machines { get; set; }
     public DbSet<Engineer> Engineers { get; set; }
     public DbSet<EngineerMachine> EngineerMachines { get; set; }
-    public FactoryContext(DbContextOptions option) : base(options) { }
+    public FactoryContext(DbContextOptions options) : base(options) { }
   }
 }
