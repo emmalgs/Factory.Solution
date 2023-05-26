cf4d14377c6863ed16fc0514b614a34a83765c96
Emma Gerigscott <elgerig@gmail.com>
2023-05-26 09:48:40 -0700

    add validation for engineer name

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage="The engineer name field cannot be empty")]
     public string Name { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
