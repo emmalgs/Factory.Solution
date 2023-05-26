ab040684e5d69c03f893fa34e87b4ae10932d4e8
Emma Gerigscott <elgerig@gmail.com>
2023-05-26 08:38:49 -0700

    add initial fields to data tables

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -2,6 +2,9 @@ namespace Factory.Models
 {
   public class Machine
   {
-    
+    public int MachineId { get; set; }
+    public string Name { get; set; }
+    public DateTime LastMaintenance { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
   }
 }
